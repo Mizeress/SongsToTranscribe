fe2dcc7c54ab9ec2739ae3e4438fa66b148c7e4c
Mizeress <karatehobbit@gmail.com>
2025-03-28 21:48:43 -0400

    Refactored code to use SongListFile and SongList classes for cleaner code and easier reuse later.

--- src/com/Mizeress/SongsToTranscribe/Main.java
+++ src/com/Mizeress/SongsToTranscribe/Main.java
@@ -1,11 +1,5 @@
 package com.Mizeress.SongsToTranscribe;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Main {
